a27f2276b86cbb83cbec4c0c9874a5fecf70105b
Demetrius81 <shizimaniak@gmail.com>
2023-03-07 18:33:48 +0300

    lesson 2 is complete

---
+++ lesson2.js
@@ -0,0 +1,87 @@
+// let age = +prompt("Введите ваш возраст");
+// console.log(age);
+// alert(`Ваш возраст ${age} лет`);
+// let userName = prompt(`Введите ваше имя`);
+// console.log(userName);
+// alert(`Добро пожаловать на сайт, ${userName}`);
+
+// let a = 13;
+// let b = 5;
+
+// console.log(a % b);
+
+// let a;
+// alert(a);
+
+// alert("abc" * 3);
+
+// alert(1 / 0);
+
+// alert(-1 / 0);
+
+// alert("2" * "3");
+
+// let a = "2";
+// let b = "3";
+// console.log(+a + +b);
+
+// a = +prompt("first number");
+// b = +prompt("second number");
+
+// console.log(`сумма ${a + b}`);
+// console.log(`разность${a - b}`);
+// console.log(`произведение${a * b}`);
+// console.log(`частное${a / b}`);
+// console.log(`остаток${a % b}`);
+
+// console.log(String(true));
+
+// console.log("a" + true);
+
+// console.log(Number(true));
+
+// console.log(true + 1);
+
+// console.log(true + true);
+
+// console.log(true - true);
+
+// console.log(String(true) + Number(true));
+
+// let a = +prompt("enter number");
+
+// if (a < 5) {
+//   console.log("Число < 5");
+// } else if (a > 5) {
+//   console.log("Число > 5");
+// } else {
+//   console.log("Число = 5");
+// }
+
+// let test1 = 3;
+// let test2 = "3";
+
+// if (test1 === test2) {
+//   console.log("Переменные равны");
+// } else {
+//   console.log("Переменные не равны");
+// }
+
+// let a = +prompt("enter first number");
+// let b = +prompt("enter second number");
+
+// if (a < b) {
+//   console.log(`Minimum is ${a}`);
+// } else if (b < a) {
+//   console.log(`Minimum is ${b}`);
+// } else {
+//   console.log(`${a} = ${b}`);
+// }
+
+// let a = +prompt("enter number");
+
+// if (a > 0 && a < 15) {
+//   console.log(`variable 0 < ${a} < 15`);
+// } else {
+//   console.log(`variable ${a} does not fit the conditions`);
+// }
