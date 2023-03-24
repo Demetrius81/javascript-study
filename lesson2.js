fd9adb7b40c05d8d99deb5cdd7ee369afe88abb3
Demetrius81 <shizimaniak@gmail.com>
2023-03-24 12:05:08 +0300

    homework is done

--- lesson2.js
+++ lesson2.js
@@ -85,3 +85,113 @@
 // } else {
 //   console.log(`variable ${a} does not fit the conditions`);
 // }
+
+// const arr = [1, 2, 3];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// console.log(arr.length);
+
+// const temp = ["a", "b", "c"];
+
+// console.log(temp);
+
+// for (let i = 0; i < temp.length; i++) {
+//   temp[i] = i + 1;
+// }
+
+// console.log(temp);
+
+// const arr = [1, 2, 3];
+
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i]++;
+// }
+
+// console.log(arr);
+
+// const arr = [];
+
+// arr[3] = "a";
+// arr[8] = "b";
+
+// console.log(arr.length);
+
+// const arr = [1, 2, 3];
+
+// for (let i = 0; i < 2; i++) {
+//   arr.push(i + 4);
+// }
+
+// console.log(arr);
+
+// const arr = [1, 2, 3, 4, 5];
+
+// for (let i = 3; i < 5; i++) {
+//   arr.pop();
+// }
+
+// console.log(arr.length);
+
+// for (let i = 11; i <= 33; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 99; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 100; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// let num = 1;
+// let count = 0;
+// while (num < 1000) {
+//   num = num * 3;
+//   count++;
+// }
+
+// console.log(num);
+// console.log(count);
+
+// const arr = [2, 5, 9, 15, 1, 4];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > 3 && arr[i] < 10) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// let sum = 0;
+// for (let i = 2; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+let str = "";
+
+for (let i = 1; i <= 9; i++) {
+  if (i == 9) {
+    str += "-" + i + "-";
+  } else {
+    str += "-" + i;
+  }
+}
+
+console.log(str);
+
+const arr = [2, 5, 9, 0, 3, 1, 4];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === 0) {
+    break;
+  }
+  console.log(arr[i]);
+}
