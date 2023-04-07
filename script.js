0163e2968e1b67f249e1b251b3fcd64a6347a58e
demetrius81 <shizimaniak@gmail.com>
2023-04-07 09:16:09 +0300

    workshop is done

--- script.js
+++ script.js
@@ -276,60 +276,230 @@ function createArray(n) {
 // С помощью метода splice преобразуйте массив в следующий:
 // [1, 4, 5]
 
-console.log("Task 4");
-console.log("-----------------------------");
+// console.log("Task 4");
+// console.log("-----------------------------");
 
-let arr = createArray(10);
+// let arr = createArray(10);
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] % 2) {
-    console.log(arr[i] ** 2);
-  } else if (arr[i] % 3 === 0) {
-    console.log(arr[i] ** 3);
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2) {
+//     console.log(arr[i] ** 2);
+//   } else if (arr[i] % 3 === 0) {
+//     console.log(arr[i] ** 3);
+//   }
+// }
+
+// console.log("-----------------------------");
+
+// arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+// ];
+// let str = "[\n";
+// for (let i = 0; i < 2; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     if (j === 0) {
+//       str = str + "\t[" + arr[j][j] + ", ";
+//     } else if (j === 2 && i !== 1) {
+//       str = str + arr[i][j] + "]\n";
+//     } else if (j == 2 && i === 1) {
+//       str = str + arr[i][j] + "]\t]";
+//     } else {
+//       str = str + arr[i][j] + ", ";
+//     }
+//   }
+// }
+
+// console.log(str);
+
+// console.log("-----------------------------");
+
+// arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
+
+// let count = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 3) {
+//     count++;
+//   }
+// }
+
+// console.log(count);
+
+// console.log("-----------------------------");
+
+// arr = [1, 2, 3, 4, 5];
+
+// count = arr.splice(1, 2);
+
+// console.log(arr);
+
+function startEnd(n = 0) {
+  if (n !== 0) {
+    console.log(`Task ${n}`);
   }
+  console.log("--------------------------------\n\n");
 }
 
-console.log("-----------------------------");
+// startEnd(1);
 
-arr = [
-  [1, 2, 3],
-  [4, 5, 6],
-];
-let str = "[\n";
-for (let i = 0; i < 2; i++) {
-  for (let j = 0; j < 3; j++) {
-    if (j === 0) {
-      str = str + "\t[" + arr[j][j] + ", ";
-    } else if (j === 2 && i !== 1) {
-      str = str + arr[i][j] + "]\n";
-    } else if (j == 2 && i === 1) {
-      str = str + arr[i][j] + "]\t]";
-    } else {
-      str = str + arr[i][j] + ", ";
+// const daysOfWeek = {
+//   1: 'Понедельник',
+//   2: 'Вторник',
+//   3: 'Среда',
+//   4: 'Четвегр',
+//   5: 'Пятница',
+//   6: 'Суббота',
+//   7: 'Воскресенье',
+// };
+
+// console.log(daysOfWeek[2]);
+
+// startEnd();
+// startEnd(2);
+
+// const user = {
+//   name: 'John',
+//   surname: 'Doe',
+//   age: 18,
+// };
+
+// console.log(`${user.name} - ${user.surname} - ${user.age}`);
+
+// startEnd(2);
+
+// // Даны два массива: первый с названиями дней недели, а второй - с их порядковыми номерами:
+// // const arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// // const arr2 = [1, 2, 3, 4, 5, 6, 7];
+// // С помощью цикла создайте объект, ключами которого будут названия дней, а значениями - их номера (или наоборот)
+
+// const arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+// const arr2 = [1, 2, 3, 4, 5, 6, 7];
+// const daysOfWeek = {};
+
+// if (arr1.length === arr2.length) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     daysOfWeek[arr1[i]] = arr2[i];
+//   }
+// }
+// console.log(daysOfWeek);
+
+// startEnd();
+
+// // const obj = {x: 1, y: 2, z: 3};
+// // Переберите этот объект циклом и возведите каждый элемент этого объекта в квадрат.
+
+// const obj = { x: 1, y: 2, z: 3 };
+
+// for (const key in obj) {
+//   obj[key] = obj[key] ** 2;
+// }
+
+// console.log(obj);
+
+// startEnd();
+
+// // * Создать объект товара
+// // id
+// // название
+// // описание
+// // цена (вывести в консоль, товар с 20% скидкой)
+
+// const product = {
+//   id: 1,
+//   name: 'Some Product',
+//   description: 'Description of some product',
+//   price: 10000,
+//   printProduct: function () {
+//     this.price *= 0.8;
+//     console.log(this);
+
+//   }
+// }
+
+// product.printProduct(3);
+
+// const obj = {
+//   key1: {
+//     key1: 1,
+//     key2: 2,
+//     key3: 3,
+//   },
+//   key2: {
+//     key1: 4,
+//     key2: 5,
+//     key3: 6,
+//   },
+//   key3: {
+//     key1: 7,
+//     key2: 8,
+//     key3: 9,
+//   },
+// }
+
+// let result = 0;
+
+// const arrObj = Object.values(obj);
+
+// for (let i = 0; i < arrObj.length; i++) {
+//   let intObj = Object.values(arrObj[i]);
+//   for (const key in intObj) {
+//     result = result + intObj[key];
+//   }
+// }
+
+// console.log(result);
+
+// result = Object.values(obj);
+// result = result.flatMap(Object.values);
+// result = result.reduce((accum, current) => accum + current, 0);
+
+
+// console.log(result);
+
+// const triangle = {
+//   sideA: 5,
+//   sideB: 6,
+//   getSqare: () => {
+//     if (this.sideA === this.sideB) {
+// console.log('error');
+//     } else {
+//       if ((sideA * 2) < sideB || (sideB * 2) < sideA) {
+//         console.log('error');
+//       }else{
+
+//       }
+
+//     }
+//   }
+// }
+
+
+// Создайте объект riddles 
+// Добавьте свойства question, answer
+// создайте метод askQuestion который задает вопрос question и сравнивает ответ с answer
+// Если ответил неверно, то в консоль выводится текст: “вы проиграли”
+// * По желанию, создать 2 подсказки, если пользователь ответил неверно
+
+const riddles = {
+  question: 'Зимой и летом одним цветом.',
+  answer: ['елка'],
+  clues: ['Растет в лесу', 'Хвойное дерево'],
+  askQuestion: function () {
+    for (let i = -1; i < this.clues.length; i++) {
+      let userAnswer = prompt(i < 0 ? this.question : this.question + this.clues[i]);
+      if (userAnswer === this.answer) {
+        alert('Молодец, угадал!')
+      } else {
+        alert((i < temp.length - 1) ? 'Не угадал. Попробуй еще.' : 'Не угадал. Игра окончена.')
+        continue;
+      }
     }
-  }
+  },
 }
 
-console.log(str);
+// console.log(riddles);
 
-console.log("-----------------------------");
+// riddles.test();
 
-arr = [1, 2, 3, 2, 4, 3, 5, 6, 3, 2, 3];
-
-let count = 0;
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === 3) {
-    count++;
-  }
-}
-
-console.log(count);
-
-console.log("-----------------------------");
-
-arr = [1, 2, 3, 4, 5];
-
-count = arr.splice(1, 2);
-
-console.log(arr);
+riddles.askQuestion();
