502d6ea9f71f9dd23eceda4c136085ff7c7a2b56
Demetrius81 <shizimaniak@gmail.com>
2023-05-16 17:01:15 +0300

    home work is ready

--- homeworks/homework11/site/scripts/script.js
+++ homeworks/homework11/site/scripts/script.js
@@ -1,22 +1,11 @@
 "use strict";
 
 function include(url) {
-    var scriptEl = document.createElement('script');
-    scriptEl.src = url;
-    document.querySelector('head').appendChild(scriptEl);
+  var scriptEl = document.createElement("script");
+  scriptEl.src = url;
+  document.querySelector("body").append(scriptEl);
 }
 
 include("scripts/Card.js");
-
-const cardsEl = document.querySelector('.shop__cards');
-
-const data = JSON.parse(dataInfo);
-
-console.log(data);
-
-for (let i = 0; i < data.length; i++) {
-    const card = new Card(data[i].imgUrl, data[i].altName, data[i].title, data[i].description, data[i].currency, data[i].price, (i + 1));
-
-    cardBoxEl.append(card.createCard());
-
-};
+include("scripts/data.js");
+include("scripts/cards.js");
