8655908d48d37e9326005068c080be0ab5d80ea5
Demetrius81 <shizimaniak@gmail.com>
2023-06-02 19:35:17 +0300

    Workshop is done

--- lessonecma1.js
+++ lessonecma1.js
@@ -43,3 +43,36 @@ const createPasswordChecker = (length) => (password) =>
 
 const sumDigits = (number) =>
   number === 0 ? 0 : (number % 10) + sumDigits(Math.floor(number / 10));
+
+const a = [1, 2, 3];
+const b = [4, 5, 6];
+
+console.log(mergeArrays(a, b));
+
+console.log(removeDuplicates(1, 2, 3, 2, 4, 1, 5));
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(arr);
+console.log(getEventNumber(arr));
+console.log(arr);
+
+console.log(calculateAverage(arr));
+
+const str = "def grh hty tgb rfg hyj";
+console.log(capitalizeFirstLetter(str));
+
+const calc1 = createCalculator(5);
+const calc2 = createCalculator(0);
+calc1.add(5);
+calc2.substract(5);
+console.log(calc1.getNumber());
+console.log(calc2.getNumber());
+
+const greeting = createGreeting("John Doe");
+greeting();
+
+const isPasswordValid = createPasswordChecker(8);
+console.log(isPasswordValid("password"));
+console.log(isPasswordValid("secret"));
+
+console.log(sumDigits(456789));
