1727b435d514aae0eb4ef2b393b225de127d53f1
Demetrius81 <shizimaniak@gmail.com>
2023-06-25 17:51:14 +0300

    workshop is ready

---
+++ vue_js/workshops/vue-workshop/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <GalleryComponent></GalleryComponent>
+  </div>
+</template>
+
+<script>
+import GalleryComponent from './components/GalleryComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    GalleryComponent,
+  },
+  data() {
+    return {
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_js/workshops/vue-workshop/src/components/HelloComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>{{ data.title }}</h1>
+        <p>{{ data.text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HelloComponent',
+    props: {
+        data: Object,
+    },
+        
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-flow: column;
+    padding: 100px;
+
+}
+
+</style>
