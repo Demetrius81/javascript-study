451a7d36f9929fa64c584d0abc2157a7ae3f8726
Demetrius81 <shizimaniak@gmail.com>
2023-06-27 17:55:25 +0300

    workshop is done

---
+++ vue_js/workshops/vue-workshop/src/components/AuthenticationComponent.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <div class="box" v-if="isAuthenticated">
+      <h3>Поздравляю, вы вошли</h3>
+    </div>
+    <div class="box" v-else>
+      <form action="#">
+        <label> <input type="text" />Введите имя </label><br />
+        <label> <input type="password" />Введите пароль </label><br />
+        <div>
+          <button type="submit">submit</button>
+          <button type="reset">reset</button><br />
+        </div>
+      </form>
+    </div>
+    <div>
+      <div class="pic" v-for="item in pics" :key="item.id">
+        <img :src="item.pic" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AuthenticationComponent",
+  props: {
+    isAuthenticated: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      pics: [
+        {
+          id: 1,
+          pic: "./img/pic1_200x200.jpeg",
+        },
+        {
+          id: 2,
+          pic: "./img/pic2_200x200.jpeg",
+        },
+        {
+          id: 3,
+          pic: "./img/pic3_200x200.jpeg",
+        },
+      ],
+    };
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.box {
+  width: 350px;
+  height: 150px;
+  background: #ccc;
+  padding: 20px;
+}
+.box form {
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  flex-flow: column;
+}
+.pic {
+  width: 200px;
+  height: 200px;
+
+  background: #ccc;
+}
+</style>
---
+++ vue_js/workshops/vue-workshop/src/components/PaymentComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-show="!pay.id === 0">
+    Ежемесячный платеж за {{ pay.id }} месяц составляет {{ pay.sum }} рублей
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaymentComponent",
+  props: {
+    pay: {
+      type: Object,
+      required: true,
+      default() {
+        return { id: 0, sum: 0 };
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
