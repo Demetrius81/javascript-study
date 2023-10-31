5090139c07ab21dce792f5f3b51393576d0ecbea
Demetrius81 <shizimaniak@gmail.com>
2023-10-31 16:55:37 +0300

    HomeWork is ready

--- js_pro/HomeWork6/scripr.js
+++ js_pro/HomeWork6/scripr.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// window.history.pushState({}, "", "index.html");
 const urlRandomPhoto =
     "https://api.unsplash.com/photos/random/?client_id=hogzj3kFFFplvMi0uANfWYsCPo1-lqgHj6UulC0KzcU";
 
@@ -9,67 +8,93 @@ pictureEl.classList.add("slide_picture-box");
 pictureEl.classList.add("img");
 const pEl = document.querySelector(".picture");
 const figcaptureEl = document.querySelector(".slide_picture-description");
+const figcaptureLikeEl = document.querySelector(".slide_picture-likes");
 const likeEl = document.querySelector(".like-btn");
 const prevEl = document.querySelector(".prev-btn");
 const nextEl = document.querySelector(".next-btn");
 const refreshEl = document.querySelector(".refresh-btn");
+
 let photosHistory = [];
-
-localStorage.setItem("test", JSON.stringify(photosHistory));
-console.log(JSON.parse(localStorage.getItem("test")));
-
 let countOfLikes = 0;
-let currentPhotoObj = {};
+let isLoadFromServer;
 let currentPhotoNum = 0;
+
 prevEl.addEventListener("click", () => {
-    localStorage.setItem("currentphotonum".JSON.stringify(--currentPhotoNum));
+    localStorage.setItem("currentphotonum", JSON.stringify(--currentPhotoNum));
     location.reload();
 });
+
 nextEl.addEventListener("click", () => {
-    localStorage.setItem("currentphotonum".JSON.stringify(++currentPhotoNum));
+    localStorage.setItem("currentphotonum", JSON.stringify(++currentPhotoNum));
+    if (currentPhotoNum === photosHistory.length - 1) {
+        isLoadFromServer = false;
+        localStorage.setItem(
+            "isloadfromserver",
+            JSON.stringify(isLoadFromServer)
+        );
+    }
     location.reload();
 });
 
 likeEl.addEventListener("click", (e) => setOrRemoveLike(likeEl));
 
 refreshEl.addEventListener("click", () => {
-    currentPhotoNum = photosHistory.keys.at(-1);
+    currentPhotoNum = photosHistory.length - 1;
     location.reload();
 });
 
-window.addEventListener("load", () => {
-    getDataFromLocalStorage();
-    if (currentPhotoNum === photosHistory.at(-1)) {
-        getPictureFromServer(urlRandomPhoto);
-        currentPhotoNum = photosHistory.keys.at(-1);
+window.addEventListener("load", async () => {
+    isLoadFromServer = JSON.parse(localStorage.getItem("isloadfromserver"));
+    await getDataFromLocalStorage(isLoadFromServer);
+    isLoadFromServer = true;
+    localStorage.setItem("isloadfromserver", JSON.stringify(isLoadFromServer));
+
+    pictureRendering(photosHistory[currentPhotoNum]);
+
+    // Выключаем кнопки при достижении конечных положений для исключения ошибки выхода за пределы массива
+    if (currentPhotoNum === 0) {
+        prevEl.disabled = true;
+        prevEl.classList.add("disabled");
+    } else {
+        prevEl.disabled = false;
+        prevEl.classList.remove("disabled");
     }
-    currentPhotoObj = photosHistory[currentPhotoNum];
-    console.log("photosHistory", photosHistory);
-    console.log("countOfLikes", countOfLikes);
-    console.log("currentPhotoObj", currentPhotoObj);
-    console.log("currentPhotoNum", currentPhotoNum);
+    if (currentPhotoNum === photosHistory.length - 1) {
+        nextEl.disabled = true;
+        nextEl.classList.add("disabled");
+    } else {
+        nextEl.disabled = false;
+        nextEl.classList.remove("disabled");
+    }
 });
 
-window.addEventListener("beforeunload", () => {
-    setDataToLocalStorage();
-    localStorage.setItem("test", "test");
+window.addEventListener("beforeunload", async () => {
+    await setDataToLocalStorage();
 });
 
 // Ставим или удаляем лайк
 function setOrRemoveLike(element) {
     if (element.classList.contains("like")) {
-        element.classList.remove("like");
-        localStorage.setItem("likes", --countOfLikes);
+        RefreshLikesData(element, --countOfLikes);
     } else {
-        element.classList.add("like");
-        localStorage.setItem("likes", ++countOfLikes);
+        RefreshLikesData(element, ++countOfLikes);
     }
+}
+
+// Изменяем данные модели лайков и отрисовку
+function RefreshLikesData(element, likes) {
+    element.classList.toggle("like");
+    localStorage.setItem("likes", likes);
+    photosHistory[currentPhotoNum].like = !photosHistory[currentPhotoNum].like;
+    figcaptureLikeEl.textContent =
+        countOfLikes === 0 ? `No likes` : `You likes ${countOfLikes} pictures`;
 }
 
 // Получаем новую картинку от сервера и сохраняем данные в модели
 async function getPictureFromServer(url) {
     const response = await fetch(url);
     const data = await response.json();
+
     const urln = new String(data.urls.regular);
     const altn = new String(data.alt_description);
     const authorn = new String(data.user.name);
@@ -81,35 +106,53 @@ async function getPictureFromServer(url) {
         author: authorn,
         like: false,
     };
+
     photosHistory.push(temp);
+    currentPhotoNum = photosHistory.length - 1;
+    await setDataToLocalStorage();
 }
 
 //Отображаем картинку
 function pictureRendering(data) {
-    // pictureEl.setAttribute("src", data.url);
-    // pictureEl.setAttribute("alt", data.alt);
-    // figcaptureEl.textContent = `Photographer: ${data.author}`;
-    // pEl.innerHTML = "";
-    // pEl.append(pictureEl);
+    pictureEl.setAttribute("src", data.url);
+    pictureEl.setAttribute("alt", data.alt);
+    figcaptureEl.textContent = `Photographer: ${data.author}`;
+    figcaptureLikeEl.textContent =
+        countOfLikes === 0 ? `No likes` : `You likes ${countOfLikes} pictures`;
+    pEl.innerHTML = "";
+    pEl.append(pictureEl);
+    data.like ? likeEl.classList.add("like") : likeEl.classList.remove("like");
 }
 
-// Получаем данные из localStorage
-function getDataFromLocalStorage() {
+// Получаем данные из localStorage или загружаем с сервера
+async function getDataFromLocalStorage(isLoadFromServer) {
     countOfLikes = +localStorage.getItem("likes") ?? 0;
     currentPhotoNum = +localStorage.getItem("currentphotonum") ?? 0;
     const strPhotos = localStorage.getItem("photos");
-    if (strPhotos !== null) {
-        photosHistory = JSON.parse(strPhotos);
+
+    if (strPhotos === null) {
+        await getPictureFromServer(urlRandomPhoto);
     } else {
-        getPictureFromServer(urlRandomPhoto);
+        photosHistory = JSON.parse(strPhotos);
+    }
+
+    if (
+        strPhotos !== null &&
+        currentPhotoNum === photosHistory.length - 1 &&
+        isLoadFromServer
+    ) {
+        await getPictureFromServer(urlRandomPhoto);
     }
 }
 
 // Сохраняем данные в localStorage
-function setDataToLocalStorage() {
+async function setDataToLocalStorage() {
     if (photosHistory !== null) {
         localStorage.setItem("likes", countOfLikes);
-        localStorage.setItem("currentphotonum".JSON.stringify(currentPhotoNum));
+        localStorage.setItem(
+            "currentphotonum",
+            JSON.stringify(currentPhotoNum)
+        );
         localStorage.setItem("photos", JSON.stringify(photosHistory));
     } else {
         throw new Error(`func setDataToLocalStorage: parameter is null`);
