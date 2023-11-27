39aef851c549b75bf211548748aa4e716c1d676b
Demetrius81 <shizimaniak@gmail.com>
2023-11-27 20:34:08 +0300

    feat: hw is ready

--- Node.js/Seminar4/index.js
+++ Node.js/Seminar4/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const joi = require("joi");
 
 const app = express();
-
 const users = [];
-
 let unuqueId = 0;
+const userSheme = joi.object({
+    firstName: joi.string().required().min(2),
+    firstName: joi.string().required().min(2),
+    secondName: joi.string().required().min(2),
+    age: joi.number().required().min(0).max(125),
+    city: joi.string().min(2),
+});
 
 app.use(express.json());
 
@@ -13,7 +19,11 @@ app.get("/users", (req, res) => {
 });
 
 app.post("/users", (req, res) => {
-    unuqueId = users.length + 1;
+    const result = userSheme.validate(req.body);
+    if (result.err) {
+        return res.status(404).send({ error: result.error.details });
+    }
+    unuqueId++;
     users.push({
         id: unuqueId,
         ...req.body,
@@ -22,6 +32,10 @@ app.post("/users", (req, res) => {
 });
 
 app.put("/users/:id", (req, res) => {
+    const result = userSheme.validate(req.body);
+    if (result.err) {
+        return res.status(404).send({ error: result.error.details });
+    }
     const id = +req.params.id;
     const user = users.find((x) => x.id === id);
     if (user) {
@@ -30,7 +44,31 @@ app.put("/users/:id", (req, res) => {
         user.secondName = secondName;
         user.age = age;
         user.city = city;
-        res.send({ user: user });
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({ user: null });
+    }
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = +req.params.id;
+    const user = users.find((x) => x.id === id);
+    if (user) {
+        res.send({ user });
+    } else {
+        res.status(404);
+        res.send({ user: null });
+    }
+});
+
+app.delete("/users/:id", (req, res) => {
+    const id = +req.params.id;
+    const user = users.find((x) => x.id === id);
+    if (user) {
+        const index = users.indexOf(user);
+        users.splice(index, 1);
+        res.send({ user });
     } else {
         res.status(404);
         res.send({ user: null });
